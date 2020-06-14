70427d979fcd3b127e058043b3e0f0ac14fa0a5e
Hubert Dworczynski <hubert.dworczynski@gmail.com>
2020-06-14 20:59:02 +0200

    simplify code, improve styles

--- src/AppHeader.tsx
+++ src/AppHeader.tsx
@@ -5,7 +5,9 @@ import "./AppHeader.scss";
 function AppHeader() {
   return (
     <header className="app-header">
-      <h1 className="app-header__title">Office location picker</h1>
+      <h1 className="app-header__title">Office location picker.</h1>
+      <p className="app-header__subtitle">Help Jamie pick his next office location!</p>
+      <p className="app-header__subtitle">Browse through the cards</p>
     </header>
   );
 }
